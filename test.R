a7252ead7bfe23c0fb479d7d7365596fe762bac7
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2015-01-12 16:34:33 -0800

    update sample to sampleSingle

--- test.R
+++ test.R
@@ -3,7 +3,7 @@ library(foRward)
 
 x=evolveWFsingle(100,0.01,0.001,0.1,0.5,0.001,c(rep(1000,100)),as.integer(runif(1,0,1e6)))
 print(WFsingleNmuts(x))
-x.sample = sample(x,10,0);
+x.sample = sampleSingle(x,10,0);
 
 print(x.sample$npos)
 print(x.sample$spos)
